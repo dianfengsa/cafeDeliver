f1a184e141dbe870ef9f3445d06f36eb29913045
李见波 <369204944@qq.com>
2016-11-15 17:27:06 +0800

    提交页面代码

--- widget/newAppJs/share.js
+++ widget/newAppJs/share.js
@@ -1,111 +1,88 @@
 /**
  * Created by binza on 2016/11/10.
  */
-$(document).ready(function() { 
-//底部栏跳转
-    $(".footer-box").on("touchend", "#footer-1", function () {
-        api.showProgress({
-            style: 'default',
-            animationType: 'fade',
-            title: '努力加载中...',
-            text: '先喝杯茶...',
-            modal: true
-        });
-        api.openWin({
-            name: 'orderDelive.html',
-            url: '../newAppHtml/orderDelive.html',
-            opaque: true, // 页面不透明
-            bounces: false, // 不允许页面弹动，ripple
-            vScrollBarEnabled: false,
-            slidBackEnabled: false,
-            animation: {
-                type: "fade", //动画类型（详见动画类型常量）
-                subType: "from_right", //动画子类型（详见动画子类型常量）
-                duration: 300 //动画过渡时间，默认300毫秒
-            },
-            delay: 0
-        });
-        api.hideProgress();
 
-        $("#footer-new1").hide();//点击后隐藏新消息图标
-    });
-    $(".footer-box").on("touchend", "#footer-2", function () {
-        api.showProgress({
-            style: 'default',
-            animationType: 'fade',
-            title: '努力加载中...',
-            text: '先喝杯茶...',
-            modal: true
-        });
-        api.openWin({
-            name: 'orderRequest',
-            url: '../newAppHtml/orderRequest.html',
-            opaque: true, // 页面不透明
-            bounces: false, // 不允许页面弹动，ripple
-            vScrollBarEnabled: false,
-            slidBackEnabled: false,
-            animation: {
-                type: "fade", //动画类型（详见动画类型常量）
-                subType: "from_right", //动画子类型（详见动画子类型常量）
-                duration: 300 //动画过渡时间，默认300毫秒
-            },
-            delay: 0
-        });
-        api.hideProgress();
+//打开外卖订单页面
+function openOrderDelive(pageParam) {
+	console.log("openOrderDelive>>>pageParam>>>>>>" + JSON.stringify(pageParam))
+	api.openFrame({
+		name : 'orderDelive',
+		url : '../newAppHtml/orderDelive.html',
+		opaque : true, // 页面不透明
+		bounces : false, // 不允许页面弹动，ripple
+		vScrollBarEnabled : false,
+		slidBackEnabled : false,
+		pageParam : pageParam,
+		animation : {
+			type : "fade", //动画类型（详见动画类型常量）
+			subType : "from_right", //动画子类型（详见动画子类型常量）
+			duration : 300 //动画过渡时间，默认300毫秒
+		},
+		delay : 0
+	});
 
-        $("#footer-new1").hide();//点击后隐藏新消息图标
-    });
-    $(".footer-box").on("touchend", "#footer-3", function () {
-        api.showProgress({
-            style: 'default',
-            animationType: 'fade',
-            title: '努力加载中...',
-            text: '先喝杯茶...',
-            modal: true
-        });
-        api.openWin({
-            name: 'orderSearch',
-            url: '../newAppHtml/orderSearch.html',
-            opaque: true, // 页面不透明
-            bounces: false, // 不允许页面弹动，ripple
-            vScrollBarEnabled: false,
-            slidBackEnabled: false,
-            animation: {
-                type: "fade", //动画类型（详见动画类型常量）
-                subType: "from_right", //动画子类型（详见动画子类型常量）
-                duration: 300 //动画过渡时间，默认300毫秒
-            },
-            delay: 0
-        });
-        api.hideProgress();
+}
 
-        $("#footer-new1").hide();//点击后隐藏新消息图标
-    });
-    $(".footer-box").on("touchend", "#footer-4", function () {
-        api.showProgress({
-            style: 'default',
-            animationType: 'fade',
-            title: '努力加载中...',
-            text: '先喝杯茶...',
-            modal: true
-        });
-        api.openWin({
-            name: 'orderDeliveInfo',
-            url: '../newAppHtml/orderDeliveInfo.html',
-            opaque: true, // 页面不透明
-            bounces: false, // 不允许页面弹动，ripple
-            vScrollBarEnabled: false,
-            slidBackEnabled: false,
-            animation: {
-                type: "fade", //动画类型（详见动画类型常量）
-                subType: "from_right", //动画子类型（详见动画子类型常量）
-                duration: 300 //动画过渡时间，默认300毫秒
-            },
-            delay: 0
-        });
-        api.hideProgress();
+//打开配送请求订单页面
+function openOrderRequest(pageParam) {
 
-        $("#footer-new1").hide();//点击后隐藏新消息图标
-    });
+	console.log("openOrderRequest>>>pageParam>>>>>>" + JSON.stringify(pageParam))
+	api.openFrame({
+		name : 'orderRequest',
+		url : '../newAppHtml/orderRequest.html',
+		opaque : true, // 页面不透明
+		bounces : false, // 不允许页面弹动，ripple
+		vScrollBarEnabled : false,
+		slidBackEnabled : false,
+		pageParam : pageParam,
+		animation : {
+			type : "fade", //动画类型（详见动画类型常量）
+			subType : "from_right", //动画子类型（详见动画子类型常量）
+			duration : 300 //动画过渡时间，默认300毫秒
+		},
+		delay : 0
+	});
+}
 
-});
+//打开搜索订单页面
+function openOrderSearch(pageParam) {
+
+	console.log("openOrderSearch>>>pageParam>>>>>>" + JSON.stringify(pageParam))
+	api.openFrame({
+		name : 'orderSearch',
+		url : '../newAppHtml/orderSearch.html',
+		opaque : true, // 页面不透明
+		bounces : false, // 不允许页面弹动，ripple
+		vScrollBarEnabled : false,
+		slidBackEnabled : false,
+		pageParam : pageParam,
+		animation : {
+			type : "fade", //动画类型（详见动画类型常量）
+			subType : "from_right", //动画子类型（详见动画子类型常量）
+			duration : 300 //动画过渡时间，默认300毫秒
+		},
+		delay : 0
+	});
+}
+
+//打开配送员信息
+function openOrderDeliveInfo(pageParam) {
+
+	console.log("openOrderDeliveInfo>>>pageParam>>>>>>" + JSON.stringify(pageParam))
+	api.openFrame({
+		name : 'orderDelive',
+		url : '../newAppHtml/orderDeliveInfo.html',
+		opaque : true, // 页面不透明
+		bounces : false, // 不允许页面弹动，ripple
+		vScrollBarEnabled : false,
+		slidBackEnabled : false,
+		pageParam : pageParam,
+		animation : {
+			type : "fade", //动画类型（详见动画类型常量）
+			subType : "from_right", //动画子类型（详见动画子类型常量）
+			duration : 300 //动画过渡时间，默认300毫秒
+		},
+		delay : 0
+	});
+}
+
